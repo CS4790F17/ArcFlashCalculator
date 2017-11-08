968eb7106452e639e949c89cf5fa284afb1efd52
Alex <mustang.4222@yahoo.com>
2017-11-08 10:23:25 -0700

    added battery, capacitor, subRF controllers and associated views

---
+++ ArcFlashCalculator/Controllers/BatteryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArcFlashCalculator.Controllers
+{
+    public class BatteryController : Controller
+    {
+        // GET: Battery
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ArcFlashCalculator/Controllers/SubRFController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ArcFlashCalculator.Controllers
+{
+    public class SubRFController : Controller
+    {
+        // GET: SubRF
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
